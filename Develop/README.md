11277db2b8ec6aa12cfb01818fea721fbbc07cff
Videl90 <lorena.sa.0117@gmail.com>
2020-06-11 01:05:18 -0500

    testing

--- Develop/README.md
+++ Develop/README.md
@@ -1,7 +1,16 @@
-
-    ksjdkslajdklajdlajdldjlajlkj Videl90
-    ldkjsaljdlakjdaldalkjdkja Hola
-    ldkajlsdjalkdjalkjdal
-    kldjasljdasldjal
-
-    
+# Project Title
+    ## Description
+    Hola, esta es la descripción
+    ## Table of Contents
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Credits](#credits)
+    -[Licence](#license)
+    ## Installation
+    Steps required to install your project
+    ## Usage
+    Instructions to use. Screenshots
+    ## Credits
+    List of collaborators
+    ## License 
+    Let the others know if they can usea your project
--- Develop/index.js
+++ Develop/index.js
@@ -49,13 +49,22 @@ function promptUserInfo(){
 
 
 function writeToFile(data) {
-    return `
-    ksjdkslajdklajdlajdldjlajlkj ${data.username}
-    ldkjsaljdlakjdaldalkjdkja ${data.title}
-    ldkajlsdjalkdjalkjdal
-    kldjasljdasldjal
-
-    `  
+    return `# Project Title
+    ## Description
+    Hola, esta es la descripción
+    ## Table of Contents
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Credits](#credits)
+    -[Licence](#license)
+    ## Installation
+    Steps required to install your project
+    ## Usage
+    Instructions to use. Screenshots
+    ## Credits
+    List of collaborators
+    ## License 
+    Let the others know if they can usea your project`  
 }
 
 async function init() {
